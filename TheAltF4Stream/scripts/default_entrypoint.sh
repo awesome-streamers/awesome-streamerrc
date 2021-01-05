ed0f319b3b5371f279dabddad10c85a171f441ba
Erik Reinert <e@drn.io>
2021-01-04 23:44:11 -0800

    thealtf4stream: updates to dockerfile

--- TheAltF4Stream/scripts/default_entrypoint.sh
+++ TheAltF4Stream/scripts/default_entrypoint.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env sh
 set -e
 
-echo -e "\nSetting neovim UID to ${UID}..." \
-  && usermod -u "${UID}" neovim \
-  && echo -e "\nStarting neovim as ${UID}:${GID}...\n" \
-  && su-exec neovim:${GID} nvim "$@"
+usermod -u "${UID}" neovim && su-exec neovim:${GID} nvim "$@"
