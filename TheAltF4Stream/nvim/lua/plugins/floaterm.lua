5aed4d2401d201c66cec3f0f2463e8f175a772bd
Erik Reinert <e@drn.io>
2021-05-21 00:31:39 -0700

    feat(floaterm)

--- TheAltF4Stream/nvim/lua/plugins/floaterm.lua
+++ TheAltF4Stream/nvim/lua/plugins/floaterm.lua
@@ -3,10 +3,10 @@ local function init()
 
   local options = { noremap = true }
 
-  map('n', '<leader>ld', '<CMD>FloatermNew --autoclose=2 --height=0.9 --width=0.9 --wintype=floating lazydocker<CR>', options)
-  map('n', '<leader>lg', '<CMD>FloatermNew --autoclose=2 --height=0.9 --width=0.9 --wintype=floating lazygit<CR>', options)
-  map('n', '<leader>rr', '<CMD>FloatermNew --autoclose=2 --height=0.75 --width=0.75 --wintype=floating ranger<CR>', options)
-  map('n', '<leader>tt', '<CMD>FloatermNew --autoclose=2 --height=0.9 --width=0.9 --wintype=floating<CR>', options)
+  map('n', '<leader>ld', '<CMD>FloatermNew --autoclose=1 --height=0.8 --width=0.8 lazydocker<CR>', options)
+  map('n', '<leader>lg', '<CMD>FloatermNew --autoclose=1 --height=0.8 --width=0.8 lazygit<CR>', options)
+  map('n', '<leader>nn', '<CMD>FloatermNew --autoclose=1 --height=0.8 --width=0.8 nnn -Hde<CR>', options)
+  map('n', '<leader>tt', '<CMD>FloatermNew --autoclose=1 --height=0.8 --width=0.8<CR>', options)
 end
 
 return {
