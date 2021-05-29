6325663a9c26e23f7e1b68e48fa8b4b66338ea8a
Erik Reinert <e@drn.io>
2021-05-28 23:14:20 -0700

    feat(keymaps)

--- TheAltF4Stream/nvim/lua/plugins/auto_session.lua
+++ TheAltF4Stream/nvim/lua/plugins/auto_session.lua
@@ -1,4 +1,8 @@
 local function init()
+  local map = vim.api.nvim_set_keymap
+
+  local mapOpts = { noremap = true }
+
   local opts = {
     auto_session_enabled = true,
     auto_save_enabled = false,
@@ -8,6 +12,10 @@ local function init()
   require'auto-session'.setup(opts)
 
   require'telescope'.load_extension("session-lens")
+
+  -- Keymaps
+  map('n', '<leader>sd', '<CMD>lua require("auto-session").DeleteSession()<CR>', mapOpts)
+  map('n', '<leader>ss', '<CMD>lua require("auto-session").SaveSession()<CR>', mapOpts)
 end
 
 return {
