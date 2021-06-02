b18403a6aaffb04ce3a0df0b9195aad8099ec994
Erik Reinert <e@drn.io>
2021-06-02 02:49:15 -0700

    feat(telescope): add lsp_workspace_diagnostics keymap

--- TheAltF4Stream/nvim/lua/TheAltF4Stream/plugins/telescope.lua
+++ TheAltF4Stream/nvim/lua/TheAltF4Stream/plugins/telescope.lua
@@ -25,6 +25,7 @@ local function init()
   map('n', '<leader>fl', '<CMD>lua require("telescope.builtin").live_grep()<CR>', options)
   map('n', '<leader>fb', '<CMD>lua require("telescope.builtin").buffers()<CR>', options)
   map('n', '<leader>fh', '<CMD>lua require("telescope.builtin").help_tags()<CR>', options)
+  map('n', '<leader>fd', '<CMD>lua require("telescope.builtin").lsp_workspace_diagnostics()<CR>', options)
 
   -- Extensions
   map('n', '<leader>fs', '<CMD>lua require("telescope").extensions["session-lens"].search_session()<CR>', options)
